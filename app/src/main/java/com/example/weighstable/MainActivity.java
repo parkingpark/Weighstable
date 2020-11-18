da03adf09c56567c881e60040570ad110394977c
Connor <chiggi25@uncc.edu>
2020-11-18 18:32:03 -0500

    got databse implemented (I hope)

--- app/src/main/java/com/example/weighstable/MainActivity.java
+++ app/src/main/java/com/example/weighstable/MainActivity.java
@@ -1,16 +1,38 @@
 package com.example.weighstable;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static String ip = "weighstable.csvpwirajhmg.us-east-2.rds.amazonaws.com";
+    private static String port = "3306";
+    private static String classes = "net.sourceforge.jtds.jdbc.Driver";
+    private static String database = "weighstable";
+    private static String username = "admin";
+    private static String password = "Group14Admin";
+    private static String url = "jdbc:jtds:sqlserver://" + ip + ":" + port + "/" + database;
+
+    private Connection connection = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +47,43 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+
+        //textView = findViewById(R.id.textView);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            Class.forName(classes);
+            connection = DriverManager.getConnection(url, username, password);
+            //textView.setText("SUCCESS");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            //textView.setText("ERROR");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+            //textView.setText("FAILURE");
+        }
+
+    }
+
+    public void sqlButton(View view) {
+        if (connection!=null) {
+            Statement statement = null;
+            try {
+                statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("Select * from TEST_TABLE;");
+                while (resultSet.next()) {
+                    //textview.setText(resultSet.getString(1));
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+        else {
+            //textView.setText("Connection is null");
+        }
+
     }
 
 }
