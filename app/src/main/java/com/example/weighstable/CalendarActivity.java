12dc4e2195b03f4f5d263235fb80d46551fab3da
CCArpe <ccarpenter6000@gmail.com>
2020-11-18 23:28:29 -0500

    Added more calendar functionality.

--- app/src/main/java/com/example/weighstable/CalendarActivity.java
+++ app/src/main/java/com/example/weighstable/CalendarActivity.java
@@ -13,10 +13,19 @@ public class CalendarActivity extends AppCompatActivity {
     CalendarView calendarView;
     TextView myData;
 
+    private Spinner mySpinner;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        mySpinner = (Spinner) findViewById(R.id.spinner1);
+        String[] days = getResources().getStringArray(R.array.names);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, days);
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
 
     }
 }
