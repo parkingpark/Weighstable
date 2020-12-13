d3f90dd2aff552c1a18c6b56808b2eb5466472db
Connor <chiggi25@uncc.edu>
2020-12-13 18:00:43 -0500

    added Firebase and Firebase Realtime DB SDK stuff

--- app/src/main/java/com/example/weighstable/DataActivity.java
+++ app/src/main/java/com/example/weighstable/DataActivity.java
@@ -21,56 +21,17 @@ import java.sql.Statement;
 
 public class DataActivity extends AppCompatActivity {
 
-    private static String ip = "weighstable.csvpwirajhmg.us-east-2.rds.amazonaws.com";
-    private static String port = "3306";
-    private static String classes = "net.sourceforge.jtds.jdbc.Driver";
-    private static String database = "weighstable";
-    private static String username = "admin";
-    private static String password = "Group14Admin";
-    private static String url = "jdbc:jtds:sqlserver://" + ip + ":" + port + "/" + database;
-    private Connection connection = null;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_data);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
-
         Button check_server = (Button) findViewById(R.id.check_server);
-        TextView connection_status = (TextView) findViewById(R.id.connection_status);
-
-        try {
-            Class.forName(classes);
-            connection = DriverManager.getConnection(url, username, password);
-            connection_status.setText("SUCCESS");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-            connection_status.setText("ERROR");
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-            connection_status.setText("FAILURE");
-        }
 
         check_server.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (connection!=null) {
-                    Statement statement = null;
-                    try {
-                        statement = connection.createStatement();
-                        ResultSet resultSet = statement.executeQuery("Select * from TEST_TABLE;");
-                        while (resultSet.next()) {
-                            connection_status.setText(resultSet.getString(1));
-                        }
-                    } catch (SQLException throwables) {
-                        throwables.printStackTrace();
-                    }
-                }
-                else {
-                    connection_status.setText("Connection is null");
-                }
+
             }
         });
 
