d999088f3ea11da67f627b46478b617c8a395e91
hperschy <hperschy@uncc.edu>
2020-12-16 19:25:31 -0500

    Implemented saving and loading Household from storage

--- app/src/main/java/com/example/weighstable/household/TrashCan.java
+++
@@ -1,9 +0,0 @@
-package com.example.weighstable.household;
-
-public class TrashCan {
-    int wLimit, cLimit;
-    public TrashCan(int wLimit, int cLimit) {
-        this.wLimit = wLimit;
-        this.cLimit = cLimit;
-    }
-}
